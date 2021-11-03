40ea03b181dd04f0f076c1a8db9fb18677d9de96
Cam Petrelli <p22cpetrelli@fairfieldprep.org>
2021-11-02 22:31:13 -0400

    add file

---
+++ hw5-4.py
@@ -0,0 +1,22 @@
+# author CJP 11/02/2021
+#Question 1
+print("Python"[2])
+
+#Question 2
+print("Strings are simply a sequence of characters."[30:32])
+
+#Question 3
+print(len("excellent"))
+
+#Question 4
+print("apple" in "Pineapple")
+
+#Question 5
+if "Peach" > "pear":
+    print("Peach does not come after")
+
+#Question 6
+#Question 7
+#Question 8
+#Question 9
+#Question 10
